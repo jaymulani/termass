cdf649b796f76bcdcef55ca77ca6acc7059911b9
Jay Jitendrakumar Mulani <jy850028@dal.ca>
2023-11-28 18:17:31 -0400

    new

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,29 +1,25 @@
 
 import React, { useState } from 'react';
 import RegistrationForm from './RegistrationForm';
-const eventsData = [
+
+const bookEventsData = [
   {
     id: 1,
-    title: 'Senior Care Day',
-    description: 'Spend time with the elderly, engage in fun activities, and bring smiles to their faces. For registering this event, click on below button and fill the details',
+    title: 'Book Club Meeting',
+    description: 'Join our monthly book club meeting to discuss the latest literary masterpiece. For registering this event, click on the button below and fill in your details.',
   },
   {
     id: 2,
-    title: 'Childrens Art Workshop',
-    description: 'Assist in organizing an art workshop for underprivileged children and inspire their creativity.For registering this event, click on below button and fill the details',
+    title: 'Library Reading Session',
+    description: 'Participate in a reading session at the local library. Bring your favorite book and share the joy of reading with fellow book lovers. For registering this event, click on the button below and fill in your details.',
   },
   {
     id: 3,
-    title: 'Fundraising Fun Run',
-    description: 'Participate in a charity run to raise funds for a noble cause.For registering this event, click on below button and fill the details',
+    title: 'Literary Symposium',
+    description: 'Immerse yourself in a captivating literary symposium, where intellects converge to explore profound literary works. To register for this event, simply click the button below and share your passion for literature.',
   },
-  {
-    id: 4,
-    title: 'Health and Wellness Fair',
-    description: 'Be part of a fair promoting healthy living and well-being in the community.For registering this event, click on below button and fill the details',
-  }, 
+  
 ];
-
 
 const Home = () => {
   const [selectedEvent, setSelectedEvent] = useState(null);
@@ -36,83 +32,82 @@ const Home = () => {
     setSelectedEvent(null);
   };
 
+  const styles = {
+    // Define your styles here
+    container: {
+      backgroundColor: '#333',
+      color: '#fff',
+      padding: '1rem',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: 'linear-gradient(to right, #333, #444)',
+    },
+    description: {
+      margin: '1rem 0',
+      padding: '1rem',
+      textAlign: 'center',
+      backgroundColor: '#f0f0f0',
+      borderRadius: '4px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    },
+    gridContainer: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+      gap: '1rem',
+      padding: '1rem',
+    },
+    card: {
+      backgroundColor: '#f0f0f0',
+      padding: '1rem',
+      borderRadius: '4px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    },
+    buttonContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: "20px",
+    },
+    registerButton: {
+      fontSize: "1.2rem",
+      padding: "10px 20px",
+      backgroundColor: "#4CAF50",
+      color: "white",
+      border: "none",
+      borderRadius: "8px",
+      cursor: "pointer",
+      outline: "none",
+      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+    },
+  };
+
   return (
     <>
       <div style={styles.container}>
-        <h1>Heartwarming Horizons Foundation</h1>
+        <h1>The Bookworm's Retreat</h1>
       </div>
       <div>
         <p style={styles.description}>
-          Welcome to the Heartwarming Horizons Foundation. We are a non-profit organization dedicated to making a positive impact in our community. Through our various events and initiatives, we aim to bring joy, support, and opportunities to those in need. Please browse through our upcoming events and join us in making a difference.
+          Welcome to the The Bookworm's Retreat. We are passionate about literature and aim to foster a love for reading in our community. Explore our upcoming book-related events and join us in celebrating the world of books.
         </p>
       </div>
       <div style={styles.gridContainer}>
-        {eventsData.map((event) => (
+        {bookEventsData.map((event) => (
           <div key={event.id} style={styles.card}>
             <h2>{event.title}</h2>
             <p>{event.description}</p>
           </div>
         ))}
       </div>
-      <h3>For volunteer registartion click here: </h3>  
+      <h3>For book registration, click here: </h3>
       <div style={styles.buttonContainer}>
-        
-      <button style={styles.registerButton} onClick={() => handleRegisterButtonClick("showForm")}>
-                Register
-              </button>
-
+        <button style={styles.registerButton} onClick={() => handleRegisterButtonClick("showForm")}>
+          Register
+        </button>
       </div>
       {selectedEvent === "showForm" && <RegistrationForm handleClose={handleCloseRegistrationForm} />}
     </>
   );
-};
-
-const styles = {
-  container: {
-    backgroundColor: '#333',
-    color: '#fff',
-    padding: '1rem',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    background: 'linear-gradient(to right, #333, #444)',
-  },
-  description: {
-    margin: '1rem 0',
-    padding: '1rem',
-    textAlign: 'center',
-    backgroundColor: '#f0f0f0',
-    borderRadius: '4px',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-  },
-  gridContainer: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-    gap: '1rem',
-    padding: '1rem',
-  },
-  card: {
-    backgroundColor: '#f0f0f0',
-    padding: '1rem',
-    borderRadius: '4px',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-  },
-  buttonContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: "20px",
-  },
-  registerButton: {
-    fontSize: "1.2rem",
-    padding: "10px 20px",
-    backgroundColor: "#4CAF50",
-    color: "white",
-    border: "none",
-    borderRadius: "8px",
-    cursor: "pointer",
-    outline: "none",
-    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
-  },
 };
 
 export default Home;
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -9,6 +9,7 @@ const RegistrationForm = ({ handleClose }) => {
     age: "",
     contactNumber: "",
     selectedEvent: "",
+    isbnNumber: "", 
   });
 
   const handleChange = (e) => {
@@ -18,25 +19,24 @@ const RegistrationForm = ({ handleClose }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-   
+
     console.log(formData);
 
     const lambdaEndpoint = process.env.REACT_APP_API_ANDPOINT;
     const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      };
-      
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    };
 
     try {
       const response = await fetch(lambdaEndpoint, requestOptions);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      window.alert('Registration successful! you will get registartion confirmation mail on your registered email with event details');
+      window.alert('Registration successful! You will receive a registration confirmation email on your registered email with event details');
       const data = await response.json();
       console.log('Lambda response:', data);
     } catch (error) {
@@ -44,6 +44,35 @@ const RegistrationForm = ({ handleClose }) => {
     }
 
     handleClose();
+  };
+
+  const styles = {
+    modal: {
+      display: "flex",
+      position: "fixed",
+      zIndex: 1,
+      left: 0,
+      top: 0,
+      width: "100%",
+      height: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    modalContent: {
+      backgroundColor: "#f0f0f0",
+      padding: "1rem",
+      borderRadius: "4px",
+      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+      width: "400px",
+    },
+    closeButton: {
+      color: "black",
+      float: "right",
+      fontSize: "1.5rem",
+      fontWeight: "bold",
+      cursor: "pointer",
+    },
   };
 
   return (
@@ -55,104 +84,93 @@ const RegistrationForm = ({ handleClose }) => {
         <h2>Registration Form</h2>
         <form onSubmit={handleSubmit}>
           <table>
-            <div>
-              <tr>
-                <td>
-                  <label>Enter your first name:</label>
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="firstName"
-                    placeholder="First Name"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                  />
-                </td>
-              </tr>
-            </div>
-            <div>
-              <tr>
-                <td>
-                  <label>Enter your last name:</label>
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="lastName"
-                    placeholder="Last Name"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                  />
-                </td>
-              </tr>
-            </div>
-            <div>
-              <tr>
-                <td>
-                  <label>Enter your email:</label>
-                </td>
-                <td>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={handleChange}
-                  />
-                </td>
-              </tr>
-            </div>
-            <div>
-              <tr>
-                <td>
-                  <label>Enter your address:</label>
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="address"
-                    placeholder="Address"
-                    value={formData.address}
-                    onChange={handleChange}
-                  />
-                </td>
-              </tr>
-            </div>
-            <div>
-              <tr>
-                <td>
-                  <label>Enter your age:</label>
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="age"
-                    placeholder="Age"
-                    value={formData.age}
-                    onChange={handleChange}
-                  />
-                </td>
-              </tr>
-            </div>
-            <div>
-              <tr>
-                <td>
-                  <label>Enter your contact Number:</label>
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="contactNumber"
-                    placeholder="Contact Number"
-                    value={formData.contactNumber}
-                    onChange={handleChange}
-                  />
-                </td>
-              </tr>
-            </div>
-            <div>
-              <tr>
+            <tr>
+              <td>
+                <label>Enter your first name:</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Enter your last name:</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Enter your email:</label>
+              </td>
+              <td>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Enter your address:</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="address"
+                  placeholder="address"
+                  value={formData.address}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Enter your age:</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="age"
+                  placeholder="Age"
+                  value={formData.age}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            
+            {/* ... other fields ... */}
+            <tr>
+              <td>
+                <label>Enter ISBN number:</label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="isbnNumber"
+                  placeholder="ISBN Number"
+                  value={formData.isbnNumber}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
                 <td>
                   <label>Select Event:</label>
                 </td>
@@ -163,20 +181,17 @@ const RegistrationForm = ({ handleClose }) => {
                     onChange={handleChange}
                   >
                     <option value="">Select an event</option>
-                    <option value="Senior Care Day">Senior Care Day</option>
-                    <option value="Childrens Art Workshop">
-                      Children's Art Workshop
+                    <option value="Book Club Meeting">Book Club Meeting</option>
+                    <option value="Library Reading Session">
+                    Library Reading Session
                     </option>
-                    <option value="Fundraising Fun Run">
-                      Fundraising Fun Run
-                    </option>
-                    <option value="Health and Wellness Fair">
-                      Health and Wellness Fair
+                    <option value="Literary Symposium">
+                    Literary Symposium
                     </option>
                   </select>
                 </td>
               </tr>
-            </div>
+    
           </table>
           <div>
             <button type="submit">Register</button>
@@ -185,35 +200,6 @@ const RegistrationForm = ({ handleClose }) => {
       </div>
     </div>
   );
-};
-
-const styles = {
-  modal: {
-    display: "flex",
-    position: "fixed",
-    zIndex: 1,
-    left: 0,
-    top: 0,
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  modalContent: {
-    backgroundColor: "#f0f0f0",
-    padding: "1rem",
-    borderRadius: "4px",
-    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-    width: "400px",
-  },
-  closeButton: {
-    color: "black",
-    float: "right",
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-    cursor: "pointer",
-  },
 };
 
 export default RegistrationForm;
